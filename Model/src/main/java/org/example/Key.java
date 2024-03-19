3ceb4eab635e6cc1b08f349aaa8df49e37fbc67d
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2024-03-19 17:19:40 +0100

    commit

--- Model/src/main/java/org/example/Key.java
+++ Model/src/main/java/org/example/Key.java
@@ -9,9 +9,9 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
 public class Key {
-    private Vector<Byte> finalKey = new Vector<Byte>();
+    private Vector<Byte> finalKey = new Vector<>();
 
-    private Map<Integer, Integer> numberOfBytes = Map.of(16, 176, 24, 208, 32, 240); // mapa ostatecznej długości klucza
+    private Map<Integer, Integer> numberOfBytes = Map.of(16, 176, 24, 208, 32, 240);
 
     private final int[] SBox = new int[]{
             0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -71,7 +71,13 @@ public class Key {
     }
 
     public byte[] keyExpansion(SecretKey secretKey){
+        byte[] keybyte= secretKey.getEncoded();
+        for(byte b:keybyte){
+            this.finalKey.add(b);
+        }
+        for(byte b:finalKey){
 
+        }
         return new byte[0];
     }
 }
